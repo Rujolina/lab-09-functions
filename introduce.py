8a79b326d729ea1436d91bf3646c146c0147bf52
Rujolina <Liliaarelizetorres@gmail.com>
2021-04-12 16:25:08 -0400

    introductions

---
+++ introduce.py
@@ -0,0 +1,7 @@
+def introduce(person1, person2):
+    print("Hello "+ str(person1) + " this is " + str(person2) + ".")
+
+p1 = input("Name a person: ")
+p2 = input("Okay, name another person: ")
+
+introduce(p1, p2)
